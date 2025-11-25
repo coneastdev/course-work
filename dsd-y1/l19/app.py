82d680f4ef2fcb8b26fb8b16548c3ec95a611e0d
coneastdev <coneastdev@proton.me>
2025-11-25 16:28:48 +0000

    update l19

--- dsd-y1/l19/app.py
+++ dsd-y1/l19/app.py
@@ -18,16 +18,46 @@ def view():
             due = datetime.strptime(loan["due"], '%Y-%m-%d')
             now = datetime.now()
             if due < now:
-                print(f"\033[0;31m{loan["loanID"]}. {loan["userName"]}({loan["userID"]}) {loan["deviceType"]} {loan["due"]} ( due )\033[0m")
+                print(f"\033[0;31m{loan["loanID"]}. {loan["userName"]} ({loan["userID"]}) {loan["deviceType"]} {loan["due"]} (due)\033[0m")
             else:
-                print(f"{loan["loanID"]}. {loan["userName"]} ( {loan["userID"]} ) {loan["deviceType"]} {loan["due"]}")
+                print(f"{loan["loanID"]}. {loan["userName"]} ({loan["userID"]}) {loan["deviceType"]} {loan["due"]}")
     selection = input("enter id to modify or enter to return $ ")
     if selection != "":
         print("")
 
 def search():
-    print("To search enter one of the following\nloan id (number)")
+    print("To search enter one of the following\n\n. loan id (numbers)\n. student ID (\"S\"+numbers)\n. student name (letters)\n. device ID (\"L-\"+3 numbers)\n")
     selection = input("Enter ")
+    chrs = list(selection)
+    results = []
+    if str(chrs[0]).isnumeric(): # loan id
+        for loan in data:
+            if str(loan["loanID"]) == selection:
+                results.append(loan)
+    elif str(chrs[0]).capitalize() == "S" and str(chrs[1]).isnumeric(): # student id
+        for loan in data:
+            if str(loan["userID"]) == selection.capitalize():
+                results.append(loan)
+    elif str(chrs[0]).capitalize() == "L" and str(chrs[1]) == "-" and str(chrs[2]).isnumeric(): # device id
+        for loan in data:
+            if loan["deviceID"] == selection.capitalize():
+                results.append(loan)
+    else: # student name
+        for loan in data:
+            if selection in str(loan["userName"]).lower():
+                results.append(loan)
+    
+    print("")
+    for loan in results:
+        due = datetime.strptime(loan["due"], '%Y-%m-%d')
+        now = datetime.now()
+        if due < now:
+            print(f"\033[0;31m{loan["loanID"]}. {loan["userName"]} ({loan["userID"]}) {loan["deviceType"]} [{loan["lent"]} / {loan["due"]}] ({loan["deviceID"]}) returned:{loan["returned"]} (due)\033[0m")
+        else:
+            print(f"{loan["loanID"]}. {loan["userName"]} ({loan["userID"]}) {loan["deviceType"]} [{loan["lent"]}] / [{loan["due"]}] ({loan["deviceID"]}) returned:{loan["returned"]}")
+    selection = input("enter id to modify or enter to return $ ")
+    if selection != "":
+        print("")
 
 def modify():
     print()
