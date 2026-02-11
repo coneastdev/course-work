27af569b4a8e9e8e4089974f63b208da1a8411a1
coneastdev <coneastdev@proton.me>
2026-02-11 12:20:09 +0000

    update esp 4a-4

--- dsd-y1/esp/4a-4/Core Employer Set Project - Task4a - Summer 2022 - Newhaven - dashboard.py
+++ dsd-y1/esp/4a-4/Core Employer Set Project - Task4a - Summer 2022 - Newhaven - dashboard.py
@@ -2,7 +2,7 @@ import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('Task_4a.csv')
+df = pd.read_csv('./dsd-y1/esp/4a-4/Task_4a.csv')
 
 def select_region() -> str:
     regions = df["Region"].unique()
@@ -24,6 +24,7 @@ def select_region() -> str:
 def show_region_trends(region):
     properties = df[df["Region"] == region]
 
+    # to get the amomount of months in csv
     width = len(df.columns) - 4
 
     for month in range((width)):
@@ -39,8 +40,46 @@ def show_region_trends(region):
 
     plt.xlabel("Months & Years")
     plt.ylabel("Avg Value")
-
     plt.title(f"Avg House Prices In {region}")
+
+    plt.show()
+
+def show_region_sizes(region):
+    properties = df[df["Region"] == region]
+
+    # average sizes by reigon
+    sizes = properties.groupby("Property Type")["Rooms"].mean()
+
+    plt.bar(sizes.index, sizes)
+
+    plt.xlabel("Protperty Types")
+    plt.ylabel("Avg Rooms")
+    plt.title(f"Avg Rooms By Property Type in {region}")
+
+    plt.show()
+
+def show_top_region_sizes():
+    avg_sizes = df.groupby("Region")["Rooms"].mean().sort_values(ascending=False)
+
+    plt.bar(avg_sizes.index, avg_sizes)
+
+    plt.xlabel("Protperty Types")
+    plt.ylabel("Avg Rooms")
+    plt.title("Property Size By Region")
+
+    plt.show()
+
+def show_regions():
+    values = df.groupby("Region")[df.columns[4:45]].mean()
+    
+    values['Average'] = values.mean(axis=1)
+    values = values.sort_values(by="Average", ascending=False)
+
+    plt.bar(values.index, values["Average"])
+
+    plt.xlabel("Regions")
+    plt.ylabel("Avg Value")
+    plt.title("Average Value Of Protperty By Region")
 
     plt.show()
 
@@ -48,47 +87,25 @@ def main_menu():
     print("\t\t****Newhaven Property Analysis Program****")
     print("1) Show region trends")
     print("2) Show region property sizes")
-    print("3) Show top regions")
+    print("3) Show top region property sizes")
+    print("4) Show top regions")
+    
     user_input = input("Enter option number or name $ ")
 
     if user_input == "1":
         region = select_region()
         show_region_trends(region)
+    elif user_input == "2":
+        region = select_region()
+        show_region_sizes(region)
+    elif user_input == "3":
+        show_top_region_sizes()
+    elif user_input == "4":
+        show_regions()
     else:
         return "Input is out of range"
     
+    main_menu()
+    
 if __name__ == "__main__":
     main_menu()
-
-# x = mainmenu()
-# while x == 1 or x == 2:
-#     if x == 1:
-#         alldata()
-
-#     elif x == 2:
-#         while True:
-#             print()
-
-#             region = input("Please enter the name of the region you would like to check:")
-#             region = region.capitalize()
-#             if region in df.Region.values:
-#                 while True:
-#                     startdate = input("PLEASE ENTER A START DATE AS MONTH-YEAR e.g. JAN-20")
-#                     startdate = startdate.capitalize()
-#                     if startdate not in df.columns:
-#                         print("Error start date not found")
-#                     else:
-#                         while True:
-#                             enddate = input("PLEASE ENTER AN END DATE AS MONTH-YEAR e.g. JAN-20")
-#                             enddate = enddate.capitalize()
-#                             if enddate not in df.columns:
-#                                 print("Error end date not found")
-#                             else:
-#                                 region_check(region, startdate, enddate)
-#                                 break
-#                         break
-#                 break
-#             else:
-#                 print("Region not found")
-
-#     x = mainmenu()
