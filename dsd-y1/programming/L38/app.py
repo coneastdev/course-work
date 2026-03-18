145bdd4fbe11b84787daa541ede418998246cdb2
coneastdev <coneastdev@proton.me>
2026-03-18 16:49:45 +0000

    update l38

--- dsd-y1/programming/L38/app.py
+++ dsd-y1/programming/L38/app.py
@@ -5,12 +5,32 @@ import numpy as np
 
 map = pd.read_excel("./dsd-y1/programming/l38/map.xlsx", header=None)
 
-COLUMNS = list(string.ascii_uppercase[:len(map.columns)])
+columns = list(string.ascii_uppercase[:len(map.columns)])
 
-map.columns = COLUMNS
+map.columns = columns
 map.index = np.arange(1, len(map) + 1)
 
 current_map = map
+map_data = {"plr": ["B", "2"], "exit": ["F", "10"]}
+
+def load_maze(level: int):
+    map = pd.read_excel("./dsd-y1/programming/l38/map.xlsx", header=None)
+
+    columns = list(string.ascii_uppercase[:len(map.columns)])
+    map.columns = columns
+    map.index = np.arange(1, len(map) + 1)
+
+    current_map = map
+    map_data = {"plr": ["B", "2"], "exit": ["F", "10"]}
+
+    plr_pos = ["A", "1"]
+
+    for col in current_map.columns:
+        pos = col.iloc("p")
+        
+
+
+    # map_data["plr"] = current_map.loc()
 
 def display_maze():
     os.system("cls")
@@ -23,7 +43,10 @@ def check_win():
     return
 
 def game():
-    plrMovement = input("Enter direction to move $ ")
+    plrMovement = input("Enter direction to move $ ").lower()
+    match plrMovement:
+        case "n":
+            print()
     game()
 
 def main():
@@ -33,6 +56,9 @@ def main():
     print("3. quit")
 
     selection = input("Enter selection number $ ")
+    if selection == "1":
+        game()
     main()
 
-print(map)
+if __name__ == "__main__":
+    main()
